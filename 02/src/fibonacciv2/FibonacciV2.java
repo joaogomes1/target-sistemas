68a4300f192292b13b885f16b98551c0f38c1454
joaogomes1 <joao.gomes45@fatec.sp.gov.br>
2023-03-04 17:10:55 -0300

    adiciona exercício 02

---
+++ 02/src/fibonacciv2/FibonacciV2.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fibonacciv2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author joaob
+ */
+public class FibonacciV2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num; // informado pelo usuário
+        
+        // coletar o número
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe um numero inteiro: ");
+        num = sc.nextInt();
+        
+        // construir a sequência de Fibonacci
+        int a;
+        int b;
+        int soma;
+        boolean encontrado;
+        
+        a = 0;
+        b = 1;
+        soma = 0;
+        encontrado = false;
+        
+        while ( soma <= num ) {
+            if ( soma == num ) {
+                encontrado = true;
+                break;
+            }
+            soma = a + b;
+            a = b;
+            b = soma;
+        }
+
+        // saídas
+        if ( encontrado ) {
+            System.out.println("o numero '" + num + "' pertence a sequencia de Fibonacci.");
+        }
+        else {
+            System.out.println("o numero '" + num + "' nao pertence a sequencia de Fibonacci.");
+        }
+        
+    } // main
+        
+} // classe
