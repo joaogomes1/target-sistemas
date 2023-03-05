d07e5563e7f3ce46166e7bc68123a63015c40aee
joaogomes1 <joao.gomes45@fatec.sp.gov.br>
2023-03-04 22:25:43 -0300

    adiciona exercício 05

---
+++ 05/05.py
@@ -0,0 +1,14 @@
+str = ""
+str_inv = ""
+
+str = input("informe uma string: ")
+
+# índice do último elemento da string informada
+i = len(str) - 1
+
+# iterar a string informada a partir de seu fim, ao passo que se popula a string invertida a partir de seu início
+while ( i >= 0 ):
+    str_inv += str[i]
+    i -= 1
+
+print(str_inv)
