6f512b98fda55f6e67774fa7adedf2a91a79efca
joaogomes1 <joao.gomes45@fatec.sp.gov.br>
2023-03-04 22:35:18 -0300

    exercício 05 - pequeno ajuste nos comentários

--- 05/05.py
+++ 05/05.py
@@ -6,7 +6,7 @@ str = input("informe uma string: ")
 # índice do último elemento da string informada
 i = len(str) - 1
 
-# iterar a string informada a partir de seu fim, ao passo que se popula a string invertida a partir de seu início
+# iterar a string informada a partir de seu fim, populando a string invertida a partir de seu início
 while ( i >= 0 ):
     str_inv += str[i]
     i -= 1
